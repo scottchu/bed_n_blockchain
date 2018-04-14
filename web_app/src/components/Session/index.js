7658827b99ec676f5424152ad36ce9a79cc3838a
Scott Chu <scottchu@icloud.com>
2018-04-14 16:25:36 -0400

    [web-app] creates Session component

---
+++ web_app/src/components/Session/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+import _ from "./style"
+
+const Session = () => {
+  return (
+    <div className={_.container}>
+      App/Session
+    </div>
+  )
+}
+
+export default Session
