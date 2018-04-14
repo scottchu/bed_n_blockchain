a89ec5fc3d63e63ddb5848b52905eabf5a74f634
Scott Chu <scottchu@icloud.com>
2018-04-14 19:50:15 -0400

    [web-app] add global css and move normalize.css into global.css.

--- web_app/src/index.js
+++ web_app/src/index.js
@@ -12,7 +12,8 @@ import store from "./store"
 // Components and styles
 // ====================================
 import App from "./components/App"
-import "normalize.css"
+
+import "./styles/global.css"
 
 render(
   <Provider store={store}>
