df2dce0ef83d251863b1203775415fc3d85907ae
Scott Chu <scottchu@icloud.com>
2018-04-19 22:39:55 -0400

    [web-app] break user session create epic into smaller pieces and use API endpoint helper

--- web_app/src/epics/user/session.js
+++ web_app/src/epics/user/session.js
@@ -1,34 +1,29 @@
 import { combineEpics } from "redux-observable"
 import { Observable } from "rxjs"
 
+import { always, compose, mapObjIndexed, path, pick, prop } from "ramda"
+
 import { TYPE, createSuccessful, createFailed } from "../../actions/user/session"
+
+import { postCreate } from "../../api/user/session"
+
+const format = compose(
+  mapObjIndexed(prop("value")),
+  pick(["email", "password"]),
+  path(["user", "session", "form"])
+)
+
+const getUserSessionForm = (getState) => () => format(getState())
 
 const create = (action$, store) => {
   return action$
     .ofType(TYPE.create)
-    .map(() => {
-      const form = store.getState().user.session.form
-
-      return {
-        email: form.email.value,
-        password: form.password.value
-      }
-    })
+    .map(getUserSessionForm(store.getState))
     .flatMap((data) => {
-      return Observable.ajax({
-        url: "http://localhost:4000/session/create",
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: {
-          form: data
-        }
-      })
+      return Observable.ajax(postCreate(data))
       .map(createSuccessful)
       .catch(createFailed)
     })
-
 }
 
 export default combineEpics(
