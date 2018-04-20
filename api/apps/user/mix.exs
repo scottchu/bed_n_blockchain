adec7207e209f66194beb2c7ce3bc53018da0adb
Scott Chu <scottchu@icloud.com>
2018-04-20 13:09:30 -0400

    [api][user] setup an ecto project and uses the share DB.Repo

---
+++ api/apps/user/mix.exs
@@ -0,0 +1,32 @@
+defmodule User.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :user,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ecto, "~> 2.1"},
+      {:db, in_umbrella: true}
+    ]
+  end
+end
