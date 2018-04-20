9c022397b5831dfbf18c57557dd437658527921d
Scott Chu <scottchu@icloud.com>
2018-04-20 13:47:33 -0400

    [api][user] add account model with changesets for account registration and password hashing

---
+++ api/apps/user/lib/user/account.ex
@@ -0,0 +1,41 @@
+defmodule User.Account do
+  use Ecto.Schema
+
+  import Ecto
+  import Ecto.Changeset
+  import Ecto.Query
+
+  schema "users" do
+    field :email,         :string
+    field :password_hash, :string
+
+    field :password,      :string, virtual: true
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:email])
+    |> validate_required([:email])
+    |> unique_constraint(:email)
+  end
+
+  def registration_changeset(struct, params \\ %{}) do
+    struct
+    |> changeset(params)
+    |> cast(params, [:password])
+    |> validate_length(:password, min: 6, max: 100)
+    |> hash_password()
+  end
+
+  def hash_password(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        password_hash = Comeonin.Argon2.hashpwsalt(password)
+
+        put_change(changeset, :password_hash, password_hash)
+
+      _ ->
+        changeset
+    end
+  end
+end
