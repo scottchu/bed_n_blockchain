94195d8b5dc58884b70b3ef2948bb4ebe1812cd3
Scott Chu <scottchu@icloud.com>
2018-04-20 13:21:13 -0400

    [api][db] create accounts table

---
+++ api/apps/db/priv/repo/migrations/20180420024959_add_accounts_table.exs
@@ -0,0 +1,12 @@
+defmodule DB.Repo.Migrations.AddAccountsTable do
+  use Ecto.Migration
+
+  def change do
+    create table("accounts") do
+      add :email, :string, null: false
+      add :password_hash, :string, null: false
+    end
+
+    create unique_index(:accounts, [:email])
+  end
+end
