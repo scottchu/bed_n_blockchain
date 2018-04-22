131aa59356a619e2d1e0610818c34432b565cb5f
Scott Chu <scottchu@icloud.com>
2018-04-22 17:49:00 -0400

    [api][user] add #create_account under User

--- api/apps/user/lib/user.ex
+++ api/apps/user/lib/user.ex
@@ -1,2 +1,11 @@
 defmodule User do
+  alias DB.Repo
+  alias User.Account
+
+  import Ecto.Query
+
+  def create_account(params \\ %{}) do
+    Account.registration_changeset(%Account{}, params)
+    |> Repo.insert()
+  end
 end
