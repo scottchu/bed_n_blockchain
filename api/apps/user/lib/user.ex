0b53858ad2e799cc2ce6ffe3521bcf2829b9e8ad
Scott Chu <scottchu@icloud.com>
2018-04-23 02:01:47 -0400

    [api][user] add authenticate function

--- api/apps/user/lib/user.ex
+++ api/apps/user/lib/user.ex
@@ -4,8 +4,25 @@ defmodule User do
 
   import Ecto.Query
 
+  def authenticate(%{"email" => email, "password" => password}) do
+    account = Repo.get_by(Account, email: String.downcase(email))
+
+    case check_password(account, password) do
+      true -> {:ok, account}
+      _ -> {:error, "Invalid email or password"}
+    end
+  end
+
+  defp check_password(account, password) do
+    case account do
+      nil -> Comeonin.Argon2.dummy_checkpw()
+      _ -> Comeonin.Argon2.checkpw(password, account.password_hash)
+    end
+  end
+
   def create_account(params \\ %{}) do
     Account.registration_changeset(%Account{}, params)
     |> Repo.insert()
   end
+
 end
