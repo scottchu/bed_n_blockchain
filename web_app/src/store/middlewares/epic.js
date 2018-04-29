e0d721fa6b17fd7d9388aad0364475105269fc7a
Scott Chu <scottchu@icloud.com>
2018-04-29 14:24:21 -0400

    [web-app] setup dependency injection for redux observable

--- web_app/src/store/middlewares/epic.js
+++ web_app/src/store/middlewares/epic.js
@@ -1,11 +1,17 @@
 // ====================================
 // Setup Redux-Observable middleware
 // ====================================
-
 import { createEpicMiddleware } from "redux-observable"
+import api from "../../api"
+import cookies from "../../lib/cookies"
 
 import epics from "../../epics"
 
-const middleware = createEpicMiddleware(epics)
+const dependencies = {
+  console,
+  cookies
+}
+
+const middleware = createEpicMiddleware(epics, { dependencies })
 
 export default middleware
