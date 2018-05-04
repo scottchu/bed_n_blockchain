0f9abec6d6adfc067d494970055c092591707ba1
Scott Chu <scottchu@icloud.com>
2018-05-04 18:46:01 -0400

    [web-app] rename Home to HomePage

--- web_app/src/components/HomePage/index.js
+++ web_app/src/components/HomePage/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 
-class Home extends Component {
+class HomePage extends Component {
   render() {
     return (
       <div id="home">
@@ -10,4 +10,4 @@ class Home extends Component {
   }
 }
 
-export default Home
+export default HomePage
