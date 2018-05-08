8cda6e1d82e48ad9822637983ace30d493514df8
Scott Chu <scottchu@icloud.com>
2018-05-08 00:46:31 -0400

    [web-app] update css styles

--- web_app/src/components/HomePage/index.js
+++ web_app/src/components/HomePage/index.js
@@ -3,7 +3,7 @@ import React, { Component } from "react"
 class HomePage extends Component {
   render() {
     return (
-      <div id="home" style={{height: 2000}}>
+      <div id="home">
         App/Home
       </div>
     )
