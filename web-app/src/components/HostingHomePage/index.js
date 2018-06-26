8590f232531eb54a9877f15e69be41bf744c0c1d
Scott Chu <scottchu@icloud.com>
2018-06-25 23:25:01 -0400

    [web-app] rename HostingPage to HostingHomePage

---
+++ web-app/src/components/HostingHomePage/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+import Hostings from "../../containers/Hostings"
+import HostingsPagination from "../../containers/HostingsPagination"
+
+import { withStyle } from "../utils/classNames"
+import style from "./style"
+
+const HostingHomePage = ({ sytle }) => {
+  return (
+    <div className={style.container}>
+      <div className={style.body}>
+        <Hostings />
+      </div>
+      <div className={style.footer}>
+        <HostingsPagination />
+      </div>
+    </div>
+  )
+}
+
+export default withStyle(style)(HostingHomePage)
