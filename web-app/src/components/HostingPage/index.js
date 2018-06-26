34fa8f0a0fae4484cfe05e1ed374f767b3b6ab6e
Scott Chu <scottchu@icloud.com>
2018-06-25 23:25:44 -0400

    [web-app] add HostingPage to route between HostingHomePage and HostingNewPage

--- web-app/src/components/HostingPage/index.js
+++ web-app/src/components/HostingPage/index.js
@@ -1,7 +1,9 @@
 import React from "react"
+import { NavLink, Route, Switch } from "react-router-dom"
 
 import AuthorizeRedirect from "../../containers/AuthorizeRedirect"
-import Hostings from "../../containers/Hostings"
+import HostingHomePage from "../HostingHomePage"
+import HostingNewPage from "../HostingNewPage"
 import HostingsPagination from "../../containers/HostingsPagination"
 
 import { withStyle } from "../utils/classNames"
@@ -10,15 +12,44 @@ import style from "./style"
 const HostingPage = ({ sytle }) => {
   return (
     <div className={style.container}>
+      {/* redirect when not signed in */}
       <AuthorizeRedirect
         when={({ signedIn }) => !signedIn}
         to="/" />
 
-      <div className={style.body}>
-        <Hostings />
+      <div className={style.header}>
+        <ul className={style.menu}>
+          <li>
+            <NavLink
+              className={style.button}
+              activeClassName={style.active}
+              exact
+              to="/host">
+              Manage My Properties
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={style.button}
+              activeClassName={style.active}
+              to="/host/new">
+              Host
+            </NavLink>
+          </li>
+        </ul>
+
       </div>
-      <div className={style.footer}>
-        <HostingsPagination />
+      <div className={style.content}>
+        <Switch>
+          <Route
+            exact path="/host"
+            component={HostingHomePage}/>
+
+          <Route
+            path="/host/new"
+            component={HostingNewPage}/>
+
+        </Switch>
       </div>
     </div>
   )
